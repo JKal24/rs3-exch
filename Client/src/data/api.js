1b5296a1259958c6100b39fb396b198d96ccb20e
JKal24 <jkalirai2499@gmail.com>
2021-07-19 22:23:19 -0400

    using docker-compose

--- Client/src/data/api.js
+++ Client/src/data/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const serverURI = process.env.SERVER_URI || 'http://localhost:5000';
+
 export const api = axios.create({
-    baseURL: 'http://localhost:8000'
+    baseURL: serverURI
 })
--- Server/server.js
+++ Server/server.js
@@ -3,11 +3,9 @@ const app = express();
 const cors = require('cors');
 const routes = require('./routes');
 
-const PORT = process.env.PORT || 8000;
-
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(PORT, () => {
+app.listen(5000, () => {
 });
