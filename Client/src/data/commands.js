4d8e7b6b75d5544d1af1c53555fd442aef88535d
JKal24 <jkalirai2499@gmail.com>
2021-07-23 01:37:02 -0400

    fixes to heroku

--- Client/src/data/commands.js
+++ Client/src/data/commands.js
@@ -1,25 +1,26 @@
 import { api } from './api';
+import axios from 'axios';
 
 export async function getItems(filter, param = '') {
     let items;
     switch (filter) {
         case 'buylimit':
-            items = (await api.get(`/BuyLimitSearch/${param}`, )).data;
+            items = (await axios.get(`/BuyLimitSearch/${param}`, )).data;
             break;
         case 'type':
-            items = (await api.get(`/SearchByTypes/${param}`)).data;
+            items = (await axios.get(`/SearchByTypes/${param}`)).data;
             break;
         case 'rising':
-            items = (await api.get('/RisingItemSearch')).data;
+            items = (await axios.get('/RisingItemSearch')).data;
             break;
         case 'falling':
-            items = (await api.get('/FallingItemSearch')).data;
+            items = (await axios.get('/FallingItemSearch')).data;
             break;
         case 'input':
-            items = (await api.get(`/SearchByKeyword/${param}`)).data;
+            items = (await axios.get(`/SearchByKeyword/${param}`)).data;
             break;
         default:
-            items = (await api.get('/RandomListing')).data;
+            items = (await axios.get('/RandomListing')).data;
 
         return items || [];
     }
@@ -28,19 +29,19 @@ export async function getItems(filter, param = '') {
 // Nav handlers
 
 export async function getBuyLimits() {
-    return (await api.get('/BuyLimitListing')).data;
+    return (await axios.get('/BuyLimitListing')).data;
 }
 
 export async function getTypes() {
-    return (await api.get('/TypeListing')).data;
+    return (await axios.get('/TypeListing')).data;
 }
 
 export async function getPageLimit() {
-    return (await api.get('/DefaultPageLimit')).data;
+    return (await axios.get('/DefaultPageLimit')).data;
 }
 
 // Update data
 
 export async function doUpdate() {
-    api.get('/Update');
+    axios.get('/Update');
 }
