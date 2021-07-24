bedc981aa7226eb6d01cb233ef6ca7e23fbae719
JKal24 <jkalirai2499@gmail.com>
2021-07-24 01:10:53 -0400

    Testing heroku postgres

--- database/index.js
+++ database/index.js
@@ -1,25 +1,22 @@
 const Pool = require('pg').Pool;
 
 let pool;
-
-if (process.env.MODE == 'production') {
-    pool = new Pool({
-        connectionString: process.env.DATABASE_URL,
-        user: process.env.PGUSER,
-        password: process.env.PGPASSWORD,
-        host: process.env.PGHOST,
-        port: process.env.PGPORT,
-        database: process.env.PGDB
-    });
-} else {
-    pool = new Pool({
-        user: 'postgres',
-        password: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        database: 'rs3items'
-    })
-}
+pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+});
+// if (process.env.MODE == 'production') {
+//     pool = new Pool({
+//         connectionString: process.env.DATABASE_URL
+//     });
+// } else {
+//     pool = new Pool({
+//         user: 'postgres',
+//         password: 'postgres',
+//         host: 'localhost',
+//         port: 5432,
+//         database: 'rs3items'
+//     })
+// }
 
 pool.on("error", (err, client) => {
     console.error("Error connecting to db", err);
