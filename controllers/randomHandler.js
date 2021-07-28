9ee978c78bf5e64f8029475a1a0393f5443280bc
JKal24 <jkalirai2499@gmail.com>
2021-07-27 22:18:18 -0400

    Express routing fixes

--- controllers/randomHandler.js
+++ controllers/randomHandler.js
@@ -1,5 +1,6 @@
 const { get_random_items } = require('../database/commands');
 
+// Page limit is determined from the server, can send any limit from any file on the page limit route
 const ITEMS_PER_PAGE = 10;
 
 module.exports = {
--- server.js
+++ server.js
@@ -1,27 +1,22 @@
-const express = require('express');
+const express = require("express");
+const PORT = process.env.PORT || 5000;
 const app = express();
-const cors = require('cors');
-const routes = require('./routes');
 const path = require('path');
+const cors = require('cors')
+app.use(cors())
 
-app.use(cors());
+const routes = require('./routes');
+app.use(routes);
+
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
-} 
-else 
-{
-    app.use(express.static(path.join(__dirname, 'client/public')));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname + '/client/build/index.html')); 
+    });
 }
-
-app.use(routes);
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
-
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`running on ${PORT}`);
