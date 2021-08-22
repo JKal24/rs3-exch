9c2cc36f6be29ff629ef4e6f07496d703f93d40e
JKal24 <jkalirai2499@gmail.com>
2021-08-21 22:58:19 -0400

    questions and acknowledgement design changes

--- client/src/pages/Information/acknowledgements.js
+++ client/src/pages/Information/acknowledgements.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import { Image } from 'react-bootstrap';
 import '../../spreadsheets/info.css';
+import wikiIcon from '../../assets/Runescape_wiki_logo.png';
+import jagexIcon from '../../assets/Jagex_Logo.png';
 
 export default function Acknowledgements() {
     return (
-        <div className='info-data'>
-            ---
-            <h2>
-                runescape.wiki ...
-            </h2>
-            <h2>
-                Jagex ...
-            </h2>
-            ---
+        <div className='acknowledgement-data'>
+            <div className="acknowledgement">
+                <Image src={wikiIcon} className="acknowledgement-image" fluid></Image>
+                <h4 className="acknowledgement-details">
+                    For providing initial data and information relating to Runescape.
+                </h4>
+            </div>
+            <div className="acknowledgement">
+                <Image src={jagexIcon} className="acknowledgement-image" fluid></Image>
+                <h4 className="acknowledgement-details">
+                    For creating the game known as Runescape.
+                </h4>
+            </div>
         </div>
     )
 }
--- client/src/pages/Information/questions.js
+++ client/src/pages/Information/questions.js
@@ -3,7 +3,7 @@ import '../../spreadsheets/info.css';
 
 export default function Questions() {
     return (
-        <div className='info-data'>
+        <div className='question-data'>
             <h3>Why is data updated so slow?</h3>
             <p>
                 Unlike Old School Runescape, there is no custom client for Runescape 3 and as a result there is no easy
@@ -13,11 +13,12 @@ export default function Questions() {
             <h3>Why is updating inactive?</h3>
             <p>
                 With Heroku I could setup heroku scheduler to update the data everyday however using the Runescape API to collect
-                large amounts of data is abysmal. I've limited my access to only collecting prices meaning I have to make 1 call per
+                large amounts of data is abysmal. I've limited my access to only collecting prices for items meaning I have to make 1 call per
                 item but it still takes hours to complete because there is a throttle of 5-10 seconds between requests.
-                The dyno hours it would cost isn't enough to keep it running constantly.
-                If you are a passerby from my github or someone I showed this to, you can run this app on your own device with
-                the source code at <a href='https://github.com/JKal24/rs3-exch'>https://github.com/JKal24/rs3-exch.</a>
+                I may still implement a heroku scheduler but I'd like to see if there are any alternatives first. I could re-build the app using
+                mediaWiki API after I've explored it a bit. As well, scraping directly from the wiki website only gave a small improvement
+                in performance so that isn't an option. If you are a passerby from my github or someone I showed this to, you can run this 
+                app on your own device with the source code at <a href='https://github.com/JKal24/rs3-exch'>https://github.com/JKal24/rs3-exch.</a>
             </p>
         </div>
     )
